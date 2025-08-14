a8225814dacbe898b99ac909ba79fa3dec2135ef
Tal Ben-Eliezer <beneliezer@gmail.com>
2025-08-14 12:03:53 -0400

    introduce cexi settings file

---
+++ settings/default/cexi.lua
@@ -0,0 +1,10 @@
+xi = xi or {}
+xi.settings = xi.settings or {}
+
+xi.settings.cexi =
+{
+    ADMIN_FRONT = "tcp://127.0.0.1:5557",
+    ADMIN_BACK  = "tcp://127.0.0.1:7001",
+    ADMIN_CONN  = "tcp://127.0.0.1:7001",  -- maps connect here
+    ZMQ_TOKEN   = "topazisawesome",        -- optional shared token
+}
