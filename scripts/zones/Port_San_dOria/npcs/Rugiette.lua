9dc2a82d82e9df9f191061d3185a1f82923bd945
Squeech007 <squeech@freenet.de>
2026-02-04 23:14:57 +0100

    Update Rugiette.lua
    removed bitmask check in onTrigger; event 746 now allowed to replay during QUEST_ACCEPTED. onEventFinish sets bit 14 and calls npcUtil.giveQuestItem to ensure badge progress increments correctly.

--- scripts/zones/Port_San_dOria/npcs/Rugiette.lua
+++ scripts/zones/Port_San_dOria/npcs/Rugiette.lua
@@ -8,12 +8,7 @@
 local entity = {}
 
 entity.onTrigger = function(player, npc)
-    local wildcatSandy = player:getCharVar('WildcatSandy')
-
-    if
-        player:getQuestStatus(xi.questLog.SANDORIA, xi.quest.id.sandoria.LURE_OF_THE_WILDCAT) == xi.questStatus.QUEST_ACCEPTED and
-        not utils.mask.getBit(wildcatSandy, 14)
-    then
+    if player:getQuestStatus(xi.questLog.SANDORIA, xi.quest.id.sandoria.LURE_OF_THE_WILDCAT) == xi.questStatus.QUEST_ACCEPTED then
         player:startEvent(746)
     else
         player:startEvent(601)
@@ -22,7 +17,11 @@ end
 
 entity.onEventFinish = function(player, csid, option, npc)
     if csid == 746 then
-        player:setCharVar('WildcatSandy', utils.mask.setBit(player:getCharVar('WildcatSandy'), 14, true))
+        local wildcatSandy = player:getCharVar('WildcatSandy')
+        if not utils.mask.getBit(wildcatSandy, 14) then
+            player:setCharVar('WildcatSandy', utils.mask.setBit(wildcatSandy, 14, true))
+            npcUtil.giveQuestItem(player, xi.questLog.SANDORIA, xi.quest.id.sandoria.LURE_OF_THE_WILDCAT, 1)
+        end
     end
 end
 
